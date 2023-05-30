74ce54abab9d44c49f12f991ca6e3affa924cc11
ricaim16 <128908127+ricaim16@users.noreply.github.com>
2023-05-30 12:16:01 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,11 +1,12 @@
 #ifndef _PRINTF_H
 #define _PRINTF_H
 
+#include <stdlib.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <limits.h>
-#include <stdlib.h>
+
 
 #define OUTPUT_BUF_SIZE 1024
 #define BUF_FLUSH -1
